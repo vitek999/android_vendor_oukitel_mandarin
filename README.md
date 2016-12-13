7ed7a7d05fc6de689705b92c5a1bc4c1ff64955f
vitek999 <vitek2012rus@gmail.com>
2016-12-13 12:39:58 +0300

    'upload'

---
+++ README.md
@@ -0,0 +1,7 @@
+Vendor Tree for CM 12.1 Oukitel k4000 Lite(mandarin)
+==================================
+Thanks to:
+@darklord4822
+@olegsvs
+@Deepflex
+@visi0nary
---
+++ mandarin-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/mandarin/vendor,system/vendor)
